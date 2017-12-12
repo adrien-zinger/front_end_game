e5dddb454988ce9d424dd182958eedf0894b8e6f
Adrien <azinger@data-inco.fr>
2017-12-12 13:40:05 +0100

    create player or get old player in game

---
+++ js/game.js
@@ -0,0 +1,64 @@
+function getPlayers() {
+    return new Promise(resolve => {
+        user.get("players").val(psVal => {
+            if (psVal === undefined) {
+                user.set({players: null}, () => {
+                    resolve(user.get("players"))
+                });
+            } else {
+                resolve(user.get("players"))
+            }
+        });
+    });
+}
+
+function getPlayer(playerName) {
+    return new Promise(resolve => {
+        getPlayers().then(ps => {
+            ps.get(playerName).val(pVal => {
+                if (pVal === undefined) {
+                    ps.put(getNewPlayer(), () => {
+                        resolve(ps.get(playerName));
+                    });
+                } else {
+                    resolve(ps.get(playerName));
+                }
+            })
+        })
+    });
+}
+
+function initPlayer(playerName) {
+    return new Promise(resolve => {
+        getPlayer(playerName).then(player => {
+            resolve(player);
+            player.put({equipment: null}, () => {
+                player.get("equipment").set({
+                    name: "hand",
+                    action: 1,
+                    type: "strentgh"
+                }, () => {
+                    player.put({starving: 30}, () => {
+                        resolve(player)
+                    });
+                });
+            });
+            //*/
+        });
+    });
+}
+
+function getNewPlayer(playerName) {
+    var ret = {};
+    ret[playerName] = {
+        name: playerName,
+        strentgh: 1,
+        agility: 1,
+        magic: 1,
+        life: 10,
+        move: 3,
+        action: 3,
+        gold: 0
+    }
+    return ret;
+}
