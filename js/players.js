763f5b219e1a385ebeb552a07cc8b76a2fb6449b
Adrien <azinger@data-inco.fr>
2017-12-21 17:25:22 +0100

    show player and prepare moves (update player = show player)

--- js/players.js
+++ js/players.js
@@ -4,27 +4,24 @@ function showPlayer(id, pos) {
         players[id] = {pos: pos};
     }
     var player = players[id];
-    if (pos.y != player.pos.y ||
-        pos.x != player.pos.x ||
-        document.getElementById(id) === undefined) {
-        var cell = document.getElementById(player.pos.x + "-"
-                                        + player.pos.y);
-        if (cell) {
-            var p = document.createElement("div");
-            p.id = id;
-            p.style.width = "16px";
-            p.style.height = "16px";
-            p.style.backgroundImage =
-                "url(img/Commissions/Engineer.png";
-            p.style.position = "absolute";
-            p.style.zIndex = 1;
-            var tmp = document.getElementById(id);
-            if (tmp) {
-                tmp.parentNode.removeChild(tmp)
-            }
-            console.log("appendChild player");
-            cell.appendChild(p);
+    var cell = document.getElementById(pos.x + "-"
+                                    + pos.y);
+    if (cell) {
+        var p = document.createElement("div");
+        p.id = id;
+        p.style.width = "16px";
+        p.style.height = "16px";
+        p.style.backgroundImage =
+            "url(img/Commissions/Engineer.png";
+        p.style.position = "absolute";
+        p.style.zIndex = 1;
+        var tmp = document.getElementById(id);
+        if (tmp) {
+            console.log("remove child");
+            tmp.parentNode.removeChild(tmp)
         }
+        console.log("appendChild player");
+        cell.appendChild(p);
     }
     players[id].pos = pos;
 }
